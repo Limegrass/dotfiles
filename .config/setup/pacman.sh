8c7a278e92bd8b8a0302e2489018b02be577b95c
Limegrass <jani17@alumni.davidson.edu>
2020-02-19 20:28:40 -0800

    Added mpdas installation
    I really need to split these things out.

--- .config/setup/pacman.sh
+++ .config/setup/pacman.sh
@@ -3,7 +3,7 @@
 sudo pacman -S --noconfirm base base-devel nodejs node \
     git python python-pip git-lfs
 sudo pacman -S --noconfirm neovim ranger ripgrep
-sudo pacman -S mpv weechat mpd ncmpcpp
+sudo pacman -S mpv weechat 
 sudo pacman -S neomutt libsasl cyrus-sasl
 # Note: use %40 for @ if using gmail in muttrc, alternative: aerc (aur).
 # Otherwise follow arch wiki
@@ -37,6 +37,15 @@ git clone https://aur.archlinux.org/sharenix-git.git
 cd sharenix-git
 makepkg -si --noconfirm
 sudo pacman -S --noconfirm gnome-screenshot
+
+# Music player daemon + client + audio scrobbler
+sudo pacman -S --noconfirm mpd ncmpcpp
+cd $HOME/git/aur
+git clone https://aur.archlinux.org/mpdas
+cd mpdas
+makepkg -si
+systemctl enable --now --user mpd
+systemctl enable --now --user mpdas
 
 # massive TeX dump because I'm lazy
 sudo pacman -S --noconfirm texlive-most texlive-lang
