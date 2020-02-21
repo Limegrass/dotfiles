3ce8d7b2b05d3da024cdddb1503aaa8de3ca5032
Limegrass <jani17@alumni.davidson.edu>
2020-02-20 21:54:42 -0800

    Added comments on getting Photoshop GPU Acceleration working
    Can't scrubby zoom without setting the virtual desktop.

--- .config/setup/wine-photoshop.sh
+++ .config/setup/wine-photoshop.sh
@@ -2,6 +2,10 @@
 cd $HOME/bin
 wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks
 chmod +x winetricks
+wget  https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks.bash-completion
+sudo mv winetricks.bash-completion /usr/share/bash-completion/completions/winetricks
 winetricks -q atmlib gdiplus msxml3 msxml6 vcrun2005 vcrun2005sp1 vcrun2008 ie6 fontsmooth-rgb gecko
+# run winecfg and set virtual desktop on.
+# Also setup nvidia drivers, PRIME, and prime-run
 
 # Uncheck Use Graphics Processor in Preferences > Performance if editing shows nothing.
