c1fa660d86b9d4745c68730fda1115a600e5e94f
James Ni <james@niis.me>
2020-06-10 12:08:02 -0700

    updated to repo init to backup on failure
    reparents existing files under a backup folder (max 1)

--- .config/setup/dotfiles-init.sh
+++ .config/setup/dotfiles-init.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 cd $HOME
-git clone --bare https://github.com/limegrass/dotfiles.git $HOME/.dotfiles
+DOTFILES_GIT_ROOT=$HOME/.dotfiles
+git clone --bare https://github.com/limegrass/dotfiles.git $DOTFILES_GIT_ROOT
 function dotfiles {
-    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+    git --git-dir=$DOTFILES_GIT_ROOT --work-tree=$HOME $@
 }
 dotfiles checkout
+if [ $? != 0 ]; then
+    BACKUP_ROOT=$DOTFILES_GIT_ROOT/backup-root
+    mkdir -p $BACKUP_ROOT
+    function reparenting_mv() {
+        source=$1;
+        target=$2;
+        mkdir -p "$target"/"$(dirname $source)"
+        mv "$source" "$target"/"$(dirname $source)"/
+    }
+    dotfiles checkout 2>&1 \
+        | egrep "\s+\." \
+        | awk {'print $1'} \
+        | xargs -I{} reparenting_mv {} $BACKUP_ROOT/;
+    echo "backed up to backup"
+fi;
+
 # TODO: Move files to a backup folder if conflicts exists.
 dotfiles config status.showUntrackedFiles no
 source $HOME/.bashrc
 source $HOME/.bash_profile
-
--- .github/README.md
+++ .github/README.md
@@ -1,5 +1,5 @@
 init through
 ```shell
-curl https://raw.githubusercontent.com/Limegrass/dotfiles/master/.config/setup/dotfiles-init.sh | bash
+curl https://raw.githubusercontent.com/Limegrass/dotfiles/master/.config/setup/dotfiles-init.sh | sh
 ```
 > .github works in GitHub and Gitea, may need to move for GitLab at some point.
