17971193e2c2f370c9467e2972259d2dbea11e50
James Ni <james@niis.me>
2020-06-21 18:17:40 -0700

    at least it works i guess
    reusable eventually

--- .config/setup/dotfiles-init.sh
+++ .config/setup/dotfiles-init.sh
@@ -1,28 +1,21 @@
-#! /bin/bash
+#! /bin/sh
 cd $HOME
 DOTFILES_GIT_ROOT=$HOME/.dotfiles
 git clone --bare https://github.com/limegrass/dotfiles.git $DOTFILES_GIT_ROOT
-function dotfiles {
+function dotfiles() {
     git --git-dir=$DOTFILES_GIT_ROOT --work-tree=$HOME $@
 }
 dotfiles checkout
 if [ $? != 0 ]; then
     BACKUP_ROOT=$DOTFILES_GIT_ROOT/backup-root
-    mkdir -p $BACKUP_ROOT
-    function reparenting_mv() {
-        source=$1;
-        target=$2;
-        mkdir -p "$target"/"$(dirname $source)"
-        mv "$source" "$target"/"$(dirname $source)"/
-    }
+    echo "backing up to $BACKUP_ROOT then retrying"
     dotfiles checkout 2>&1 \
         | egrep "\s+\." \
         | awk {'print $1'} \
-        | xargs -I{} reparenting_mv {} $BACKUP_ROOT/;
-    echo "backed up to backup"
+        | xargs -I{} sh -c "echo {}; cp -r {} $BACKUP_ROOT/{}; rm {};";
+    dotfiles checkout
 fi;
 
-# TODO: Move files to a backup folder if conflicts exists.
 dotfiles config status.showUntrackedFiles no
 source $HOME/.bashrc
 source $HOME/.bash_profile
