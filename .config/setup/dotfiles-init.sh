e2c40fd8c6dce8a6c8537a798fc4a4ad80cdcd85
James Ni <james@niis.me>
2020-10-31 12:44:44 -0700

    init: removed bashisms and fixed backup
    now it actually just works(?)

--- .config/setup/dotfiles-init.sh
+++ .config/setup/dotfiles-init.sh
@@ -2,20 +2,24 @@
 cd $HOME
 DOTFILES_GIT_ROOT=$HOME/.dotfiles
 git clone --bare https://github.com/limegrass/dotfiles.git $DOTFILES_GIT_ROOT
-function dotfiles() {
+dotfiles() {
     git --git-dir=$DOTFILES_GIT_ROOT --work-tree=$HOME $@
 }
 dotfiles checkout
 if [ $? != 0 ]; then
     BACKUP_ROOT=$DOTFILES_GIT_ROOT/backup-root
+    mkdir $BACKUP_ROOT
     echo "backing up to $BACKUP_ROOT then retrying"
     dotfiles checkout 2>&1 \
-        | egrep "\s+\." \
+        | grep -E "\s+\." \
         | awk {'print $1'} \
-        | xargs -I{} sh -c "echo {}; cp -r {} $BACKUP_ROOT/{}; rm {};";
+        | xargs -I{} sh -c "
+            dirname {} | xargs -I{-} mkdir -p $BACKUP_ROOT/{-};
+            cp {} $BACKUP_ROOT/{};
+            rm {};";
     dotfiles checkout
 fi;
 
 dotfiles config status.showUntrackedFiles no
-source $HOME/.bashrc
-source $HOME/.bash_profile
+. $HOME/.config/sh/aliases.sh
+. $HOME/.config/sh/profile.sh
