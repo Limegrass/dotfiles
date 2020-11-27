cf499699a0b5b470130ca514a1df7b80470d3657
Limegrass <james@niis.me>
2020-11-27 22:07:52 +0000

    fixed conditions, posix
    which -> command -v
    -e test was failing as well

--- .config/sh/profile.sh
+++ .config/sh/profile.sh
@@ -1,15 +1,15 @@
 export XDG_CONFIG_HOME=$HOME/.config # default
 export PATH="$HOME/bin:$HOME/.cargo/bin:$PATH:$XDG_CONFIG_HOME/bin"
-export EDITOR="$(which nvim)"
+export EDITOR="$(command -v nvim || command -v vim)"
 export VISUAL="$EDITOR"
 export VIMINIT="source $XDG_CONFIG_HOME/vim/vimrc" # Cross platform and nvim/vim agnostic
 export GVIMINIT="source $XDG_CONFIG_HOME"/vim/gvimrc
-export SSH_ASKPASS="ssh-askpass"
+export SSH_ASKPASS="$(command -v ssh-askpass)"
 export INPUTRC="$XDG_CONFIG_HOME"/readline/inputrc
 export WINIT_X11_SCALE_FACTOR=1 # no scaling by screen dpi
 
-export FZF_DEFAULT_COMMAND=" $([ -e `which rg` ] \
-    && echo `which rg` --smart-case --hidden --no-ignore-vcs)"
+export FZF_DEFAULT_COMMAND="$([ $(command -v rg) ] \
+    && echo `command -v rg` --smart-case --hidden --no-ignore-vcs)"
 export FZF_DEFAULT_OPTS="
     --layout=reverse
     --info=inline
