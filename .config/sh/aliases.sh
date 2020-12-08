87ed5e48901f7244922cb80a00b4d52d8724de1c
Limegrass <james@niis.me>
2020-12-08 19:13:08 +0000

    added tree alias -> exa --tree

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -2,3 +2,4 @@ alias ls='ls --color=auto'
 alias xclipboard="xclip -selection clipboard"
 alias dotfiles="git --git-dir='$HOME/.dotfiles/' --work-tree='$HOME'"
 alias tmux='tmux -2'
+alias tree="$( ([ $(command -v exa) ] && echo "$(command -v exa)" --tree) || command -v tree )"
