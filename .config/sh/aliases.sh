93dcdf28822d8d10fc7f6a61e963e1aa90957c3c
Limegrass <james@niis.me>
2021-08-23 17:54:30 +0000

    fix: shellcheck alias definitions

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -1,5 +1,9 @@
+#! /bin/sh
+
 alias ls='ls --color=auto'
 alias xclipboard="xclip -selection clipboard"
+# shellcheck disable=2139
 alias dotfiles="git --git-dir='$HOME/.dotfiles/' --work-tree='$HOME'"
 alias tmux='tmux -2'
-alias tree="$( ([ $(command -v exa) ] && echo "$(command -v exa)" --tree) || command -v tree )"
+# shellcheck disable=2139
+alias tree="$( ([ "$(command -v exa)" ] && echo "$(command -v exa)" --tree) || command -v tree )"
