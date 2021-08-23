2256a9b70c552bcd67407e03457ab690b5854745
Limegrass <james@niis.me>
2021-08-23 17:55:06 +0000

    feat(alias): g -> git
    can't imagine any other command I would use more often than git
    which is used often enough that a single letter alias is nice.

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -7,3 +7,4 @@ alias dotfiles="git --git-dir='$HOME/.dotfiles/' --work-tree='$HOME'"
 alias tmux='tmux -2'
 # shellcheck disable=2139
 alias tree="$( ([ "$(command -v exa)" ] && echo "$(command -v exa)" --tree) || command -v tree )"
+alias g="git"
