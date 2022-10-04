09cfedfc52c71ade261a983c30c7faf4cc5bc778
Limegrass <james@niis.me>
2022-10-04 00:33:03 +0000

    feat: switch to eslint_d, prettierd
    The base servers were noticable slow.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -159,15 +159,15 @@ return require("packer").startup(function(use)
             local null_ls = require("null-ls")
             null_ls.setup({
                 sources = {
-                    null_ls.builtins.code_actions.eslint,
+                    null_ls.builtins.code_actions.eslint_d,
                     null_ls.builtins.code_actions.gitsigns,
                     null_ls.builtins.code_actions.shellcheck,
                     null_ls.builtins.diagnostics.cfn_lint,
                     null_ls.builtins.diagnostics.commitlint,
-                    null_ls.builtins.diagnostics.eslint,
+                    null_ls.builtins.diagnostics.eslint_d,
                     null_ls.builtins.diagnostics.shellcheck,
-                    null_ls.builtins.formatting.eslint,
-                    null_ls.builtins.formatting.prettier,
+                    null_ls.builtins.formatting.eslint_d,
+                    null_ls.builtins.formatting.prettierd,
                 }
             })
         end,
