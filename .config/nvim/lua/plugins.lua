fe06fa675f4a80893760d5b8308dd069062637c6
James <james@niis.me>
2022-10-07 19:03:59 -0700

    feat: auto-format on save
    implementation may be buggy, unsure

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -152,7 +152,27 @@ return require("packer").startup(function(use)
         "jose-elias-alvarez/null-ls.nvim",
         config = function()
             local null_ls = require("null-ls")
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
             null_ls.setup({
+                -- you can reuse a shared lspconfig on_attach callback here
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                if vim.lsp.buf.formatting ~= nil then
+                                    vim.lsp.buf.formatting()
+                                elseif vim.lsp.buf.format ~= nil then
+                                    vim.lsp.buf.format({
+                                        bufnr = bufnr,
+                                    })
+                                end
+                            end,
+                        })
+                    end
+                end,
                 sources = {
                     null_ls.builtins.code_actions.eslint_d,
                     null_ls.builtins.code_actions.gitsigns,
