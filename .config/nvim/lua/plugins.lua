6c171e3b02f22aa9f7c136150aeccd24e063fe86
James <james@niis.me>
2022-12-28 17:37:50 -0800

    fix(nvim): add vim lua files to sumneko_lua workspace

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -486,7 +486,13 @@ return require("packer").startup(function(use)
                         Lua = {
                             diagnostics = {
                                 globals = { "vim" }
-                            }
+                            },
+                            workspace = {
+                                library = {
+                                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+                                }
+                            },
                         },
                     },
                 },
