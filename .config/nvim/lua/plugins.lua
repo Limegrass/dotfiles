4984d3174b871a33b1b3a4bef3fc747a1c498467
Limegrass <james@niis.me>
2022-12-29 00:57:11 +0000

    fix: remove never-used gitsigns code actions

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -177,7 +177,6 @@ return require("packer").startup(function(use)
                 end,
                 sources = {
                     null_ls.builtins.code_actions.eslint_d,
-                    null_ls.builtins.code_actions.gitsigns,
                     null_ls.builtins.code_actions.shellcheck,
                     null_ls.builtins.diagnostics.cfn_lint,
                     null_ls.builtins.diagnostics.commitlint,
