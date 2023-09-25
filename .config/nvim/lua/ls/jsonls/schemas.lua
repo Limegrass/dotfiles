fbd5773b90fe8bfcc2f2a9f11dbfb3b91fc16e57
Limegrass <james@niis.me>
2023-09-25 19:41:14 +0000

    refactor: use schema_store module in ls.jsonls.schemas

--- .config/nvim/lua/ls/jsonls/schemas.lua
+++ .config/nvim/lua/ls/jsonls/schemas.lua
@@ -1,18 +1,5 @@
-local SCHEMA_STORE_CATALOG_PATH = vim.env.VIMLOCAL .. "/schemastore.catalog.json"
-if vim.fn.filereadable(SCHEMA_STORE_CATALOG_PATH) == 0 and vim.fn.executable("curl") > 0 then
-    vim.fn.system({
-        "curl",
-        "--output",
-        SCHEMA_STORE_CATALOG_PATH,
-        "https://raw.githubusercontent.com/SchemaStore/schemastore/master/src/api/json/catalog.json",
-    })
-end
-
-if vim.fn.filereadable(SCHEMA_STORE_CATALOG_PATH) > 0 then
-    local file_content_lines = vim.fn.readfile(SCHEMA_STORE_CATALOG_PATH)
-    local file_content = vim.fn.join(file_content_lines, "")
-    SCHEMA_STORE_CATALOG = vim.json.decode(file_content) or { schemas = {} }
-end
+package.path = package.path .. ";../?.lua"
+local schema_store_catalog = require("schema_store").catalog
 
 local CUSTOM_SCHEMAS = {
     {
@@ -85,4 +72,6 @@ local CUSTOM_SCHEMAS = {
     },
 }
 
-return vim.tbl_deep_extend("keep", CUSTOM_SCHEMAS, SCHEMA_STORE_CATALOG["schemas"])
+local combined_schemas = vim.tbl_deep_extend("keep", CUSTOM_SCHEMAS, schema_store_catalog["schemas"])
+
+return combined_schemas
