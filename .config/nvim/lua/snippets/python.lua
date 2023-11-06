21e79d625430ad15773996c9fb21afa532595b8c
James <james@niis.me>
2023-11-05 16:24:18 -0800

    feat(nvim): add python try, lambda, and with_as snippets

--- .config/nvim/lua/snippets/python.lua
+++ .config/nvim/lua/snippets/python.lua
@@ -7,6 +7,58 @@ end)
 
 local is_only_trigger = line_begin_show_condition + conditions_show.line_end
 
+local try_except_node = fmt([[
+try:
+	{}
+except:
+	{}
+]], {
+    insert_node(1, "pass"),
+    insert_node(0, "pass"),
+})
+
+local try_except_finally_node = fmt([[
+try:
+	{}
+except:
+	{}
+finally:
+	{}
+]], {
+    insert_node(1, "pass"),
+    insert_node(2, "pass"),
+    insert_node(0, "pass"),
+})
+
+local try_except_snippet = snippet(
+    "try_except",
+    choice_node(1, {
+        try_except_node,
+        try_except_finally_node,
+    })
+)
+
+local lambda_snippet = snippet("lambda", {
+    unpack(fmt("lambda {}: {}", {
+        insert_node(1, "params"),
+        insert_node(0, "expr"),
+    }))
+})
+
+local with_as_node = fmt([[
+with {context} as {ident}:
+	{body}
+]], {
+    context = choice_node(1, {
+        insert_node(nil, "context"),
+        fmt("open(\"{}\")", { insert_node(1, "file_path") }),
+    }),
+    ident = insert_node(2, "ident"),
+    body = insert_node(0, "pass"),
+})
+
+local with_as_snippet = snippet("with_as", with_as_node)
+
 return {
     snippet("__main__", {
         indent_snippet_node(
@@ -30,4 +82,7 @@ return {
             value = insert_node(0),
         })
     ),
+    try_except_snippet,
+    lambda_snippet,
+    with_as_snippet,
 }
