421e8fd92271158ab2fc20525826357753a7f08d
James <james@niis.me>
2025-01-26 18:32:37 -0800

    feat(river): set csd for steam games

--- .config/river/rules.sh
+++ .config/river/rules.sh
@@ -5,3 +5,5 @@ all_tags=$(((1 << 32) - 1))
 riverctl rule-add -title "Picture-in-Picture" float
 riverctl rule-add -title "Picture-in-Picture" csd
 riverctl rule-add -title "Picture-in-Picture" tags $all_tags
+
+riverctl rule-add -app-id "steam_app_*" csd # csd steam games
