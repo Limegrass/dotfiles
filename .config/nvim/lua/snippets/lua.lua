1579f6f2bb6cc7c209198e74a90afc9cd29c3e84
Limegrass <james@niis.me>
2025-02-25 00:39:59 +0000

    feat(nvim): add nvim lua map snippet

--- .config/nvim/lua/snippets/lua.lua
+++ .config/nvim/lua/snippets/lua.lua
@@ -9,4 +9,26 @@ return {
             }
         )
     ),
+    snippet(
+        "map",
+        fmta(
+            "vim.keymap.set(<mode>, \"<keybind>\", <fn>, {<opts>})",
+            {
+                mode = choice_node(1, {
+                    fmta("\"<mode>\"", { mode = insert_node(1) }),
+                    fmta("{ <mode> }", {
+                        mode = insert_node(1),
+                    }),
+                }),
+                keybind = insert_node(2, "keybind"),
+                fn = choice_node(3, {
+                    insert_node(nil, "fn"),
+                    fmta("function() <fn_def> end", {
+                        fn_def = insert_node(1, "fn_def"),
+                    }),
+                }),
+                opts = insert_node(4, nil),
+            }
+        )
+    ),
 }
