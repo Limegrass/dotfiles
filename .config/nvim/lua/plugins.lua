739ce49dcc9286384985ac3771c53b159801b760
James <james@niis.me>
2025-05-16 01:37:41 -0700

    fix(nvim): configure no lazy and priority on onedark colorscheme
    It's suggested to add these properties to your main
    colorscheme from the lazy documentation.

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -363,6 +363,8 @@ return {
 
     {
         "navarasu/onedark.nvim",
+        lazy = false,
+        priority = 1000,
         config = function()
             require("onedark").setup({
                 style = "dark",
