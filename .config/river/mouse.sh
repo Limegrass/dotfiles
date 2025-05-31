6516577bdec26e5c0e21ecdb23ea75e8462268b3
James <james@niis.me>
2025-05-31 00:23:09 -0700

    fix(river): use flat accel-profile for all G-Wolves pointers
    New mouse with Fenrir and never want mouse accel for any mice.
    Maybe all pointers can be flat, but not sure if I had a reason
    against that like preferring it has some for qmk mouse controls.

--- .config/river/mouse.sh
+++ .config/river/mouse.sh
@@ -5,4 +5,4 @@
 # HSK Plus pointer-13284-170-Compx_2.4G_Wireless_Receiver
 # flat = 1:1 response, so no accel
 riverctl list-inputs | rg pointer-.*Receiver | xargs -I{} riverctl input {} accel-profile flat;
-riverctl list-inputs | rg pointer-.*G-Wolves_HSK.* | xargs -I{} riverctl input {} accel-profile flat;
+riverctl list-inputs | rg pointer-.*G-Wolves_.* | xargs -I{} riverctl input {} accel-profile flat;
