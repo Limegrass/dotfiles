becca8eaef1ab1a13614b023805ba9ec85a3ed19
James <james@niis.me>
2026-03-04 22:10:59 -0800

    style: remove equals in dotfiles alias
    I don't remember why I did this but it was sitting locally.

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -4,7 +4,7 @@
 alias ls="$( ([ "$(command -v exa)" ] && echo "$(command -v exa)" -al) || echo ls -al --color=auto )"
 alias xclipboard="xclip -selection clipboard"
 # shellcheck disable=2139
-alias dotfiles="git --git-dir='$HOME/.dotfiles/' --work-tree='$HOME'"
+alias dotfiles="git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
 alias tmux='tmux -2'
 # shellcheck disable=2139
 alias tree="$( ([ "$(command -v exa)" ] && echo "$(command -v exa)" --tree) || command -v tree )"
